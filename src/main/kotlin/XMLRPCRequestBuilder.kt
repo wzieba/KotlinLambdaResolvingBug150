991e2b7726805e8e9c188d362b4fc4ea690363e1
Wojtek Zieba <wojtek.zieba@automattic.com>
2022-04-04 17:33:55 +0200

    Simplify reproduction

--- src/main/kotlin/XMLRPCRequestBuilder.kt
+++ src/main/kotlin/XMLRPCRequestBuilder.kt
@@ -3,14 +3,14 @@ class XMLRPCRequestBuilder {
     fun passingBuildGetRequest(): XMLRPCRequest {
         return XMLRPCRequest(object : Listener<Any> {
             override fun onResponse(obj: Any?) {
-                Map::class.java.cast(obj)?.let { println(it) }
+
             }
         })
     }
 
     fun failingBuildGetRequest(): XMLRPCRequest {
         return XMLRPCRequest { obj: Any? ->
-            Map::class.java.cast(obj)?.let { println(it) }
+            
         }
     }
 }
